d8beb0b84d5b220f7baafc1ace14bf9c468baef5
CarlosM30 <mucarlosalberto@gmail.com>
2025-03-24 06:48:58 -0600

    Comentado

--- src/Login.js
+++ src/Login.js
@@ -2,16 +2,39 @@ import React, { useState } from 'react';
 import styles from './Login.module.css';
 import { useNavigate } from 'react-router-dom';
 
+/**
+ * Componente de inicio de sesión.
+ * 
+ * Este componente permite a los usuarios ingresar su nombre de usuario y contraseña 
+ * para auntenticarse en la aplicación. Dependiendo del rol del usuario, se redirige
+ * a una página especifica (Chef, Encargado de Inventario, Gerente)
+ * 
+ * @component 
+ * @returns {JSX.Element} El formulario de inicion de sessión.
+ */
 const Login = () => {
 
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
 
+  //Estados del componente
+  const [username, setUsername] = useState(''); // Almacena el nombre de usuario
+  const [password, setPassword] = useState(''); // Almacena la contraseña
+  const [showPassword, setShowPassword] = useState(false); // Controla si se muestra la contraseña
+  const [errorMessage, setErrorMessage] = useState(''); // Almacena mensajes de Error
+
+  /**
+   * Maneja el envío del formulario de inición de sessión
+   * 
+   * Realiza una solicitud HTTP POST al servidor para autentificar al usuario.
+   * Si la auntentificación es exitosa envia al usuario a una pagina especifica
+   * según su rol. Si falla, se muestra un mensaje de error.
+   * 
+   * @param {Event} e - El evento de envío de formulario. 
+   */
   const handleLogin = (e) => {
     e.preventDefault();
+
+    //Realiza la solicitud al servidor
     fetch('http://localhost/MakiManage/login.php', {
       method: 'POST',
       headers: {
@@ -24,7 +47,7 @@ const Login = () => {
       if (data.status === 'success') {
         alert(`¡Bienvenido! Especialidad: ${data.Especialidad}, Estado: ${data.Estado}`);
 
-        // Verificar el rol y redirigir a la página correspondiente
+        //Redirige al usuario según su rol
         if (data.Especialidad === 'Chef') {
           navigate('/inventory');
         } else if (data.Especialidad === 'Encargado de Inventario') {
@@ -45,6 +68,7 @@ const Login = () => {
       setErrorMessage('Error en la conexión con el servidor.');
     });
   };
+
   return (
     <div className={styles.bodyContainer}>
       <div className={styles.loginContainer}>
@@ -52,8 +76,13 @@ const Login = () => {
           <div className={styles.avatar}>
           <img src="http://localhost/MakiManage/Avatar.png" alt="Avatar" />
           </div>
+
+          {/* Titulo del formulario */}
           <h2>Iniciar Sesión</h2>
+
+          {/* Formulario de inicio de sesión */}
           <form onSubmit={handleLogin}>
+            {/* Campo de nombre de usuario */}
             <div className={styles.inputContainer}>
               <label>Usuario</label>
               <input 
@@ -63,6 +92,8 @@ const Login = () => {
                 required 
               />
             </div>
+
+            {/* Campo de contraseña */}
             <div className={styles.inputContainer}>
               <label>Contraseña</label>
               <input 
@@ -71,19 +102,26 @@ const Login = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 required 
               />
+
+              {/* Checkbox para mostras/ocultar la contraseña */}
               <div className={styles.showPasswordContainer}>
-              <label>Mostrar</label>
-              <label>Contraseña</label>
+                <label>Mostrar</label>
+                <label>Contraseña</label>
                 <input 
                   type="checkbox" 
                   checked={showPassword}
                   onChange={() => setShowPassword(!showPassword)}
                 />
-                
               </div>
             </div>
+
+            {/* Botón de inicio de sesión */}
             <button className={styles.loginBtn} type="submit">INGRESAR</button>
+
+            {/* Botón de registro */}
             <button className={styles.registerBtn} type="button" onClick={() => navigate('/register')}>REGISTRAR</button>
+            
+            {/* Mensaje de error */}
             {errorMessage && <p className={styles.message}>{errorMessage}</p>}
           </form>
         </div>
